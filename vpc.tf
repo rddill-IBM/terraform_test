4e9be7c7c38080d45c6856bf5f941d45872696f9
Bob Dill <rddill@us.ibm.com>
2020-03-31 09:53:56 -0400

    update keys specification

--- vpc.tf
+++ vpc.tf
@@ -56,7 +56,7 @@ resource ibm_is_instance "vsi1" {
   resource_group = "${data.ibm_resource_group.group.id}"
   vpc     = "${ibm_is_vpc.vpc.id}"
   zone    = "${local.ZONE}"
-  keys    = ["${data.ibm_is_ssh_key.ssh_key_id.id}"]
+  keys    = ["${data.ibm_is_ssh_key.ssh_key.id}"]
   image   = "${data.ibm_is_image.ubuntu.id}"
   profile = "cc1-2x4"
 
